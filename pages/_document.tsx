74c6f53f116f25789356e58e2740eddc89c4c8d5
Bhushan Shah <bhushan.shah.dev@gmail.com>
2021-09-18 17:38:35 -0700

    Added lang attribute to html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en-US">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=optional"
